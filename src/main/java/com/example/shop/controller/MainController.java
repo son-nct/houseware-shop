93507d273ea7724a366862b07f8c3606be0838e5
xuatmon01 <sonnctse140371@fpt.edu.vn>
2021-08-14 18:23:05 +0700

    update add to cart

--- src/main/java/com/example/shop/controller/MainController.java
+++ src/main/java/com/example/shop/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.example.shop.controller;
 
+import com.example.shop.entity.Cart;
 import com.example.shop.entity.Category;
 import com.example.shop.entity.Image;
 import com.example.shop.entity.Product;
@@ -15,6 +16,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.persistence.GeneratedValue;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -89,9 +92,60 @@ public class MainController {
         return "detail";
     }
 
+    @GetMapping("/add-to-cart")
+    public String addToCart(Model model, HttpSession session, @RequestParam("productId")Long productId) {
+        Product product = productRepository.getProductById(productId);
+
+        //để dùng dc session thì gọi đến httpSession
+        Cart cart = new Cart();
+        cart.setProductId(productId);
+        cart.setProductCode(product.getCode());
+        cart.setProductName(product.getName());
+        cart.setProductQuantity(product.getQuantity());
+        cart.setProductPrice(product.getPrice());
+        cart.setProductDescription(product.getDescription());
+        cart.setProductImageUrl(product.getImageUrl());
+        cart.setQuantity(1);
+
+        List<Cart>listCart = ( List<Cart>)session.getAttribute("CART");
+
+        if(listCart == null) {
+            listCart = new ArrayList<>();
+            listCart.add(cart);
+        }else {
+            boolean isExist = false;
+            for (Cart c: listCart ) {
+                if(c.getProductId().equals(productId)) {
+                    isExist = true;
+                    c.setQuantity(c.getQuantity() + 1);
+                }
+
+            }
+            if(!isExist) {
+                listCart.add(cart);
+            }
+
+        }
+        session.setAttribute("CART",listCart);
+        return "redirect:/carts";
+    }
+
     @GetMapping("/carts")
-    public String listCart() {
-        return "listCart";
+    public String listCart(HttpSession session, Model model) {
+        List<Cart>listCart = (List<Cart>)session.getAttribute("CART");
+        if(listCart == null || listCart.size() == 0) {
+            return "emptyCart";
+        }else {
+            double totalMoney = 0;
+            for (Cart cart: listCart ) {
+                totalMoney += cart.getQuantity() * cart.getProductPrice();
+            }
+            totalMoney = Math.ceil(totalMoney * 100) / 100;
+
+            model.addAttribute("totalMoney",totalMoney);
+            model.addAttribute("listCart",listCart);
+            return "listCart";
+        }
     }
 
     @GetMapping("/checkout")
