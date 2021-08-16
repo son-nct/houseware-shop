5c12f989cb8ac03e1abc7ea15eed48f976d62247
xuatmon01 <sonnctse140371@fpt.edu.vn>
2021-08-16 18:35:19 +0700

    fix paging but lack of paging when search with keyword

--- src/main/java/com/example/shop/controller/MainController.java
+++ src/main/java/com/example/shop/controller/MainController.java
@@ -4,6 +4,7 @@ import com.example.shop.entity.*;
 import com.example.shop.repository.CategoryRepository;
 import com.example.shop.repository.ImageRepository;
 import com.example.shop.repository.ProductRepository;
+import com.example.shop.repository.ShippingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -11,8 +12,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.persistence.GeneratedValue;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +32,9 @@ public class MainController {
     @Autowired
     CategoryRepository categoryRepository;
 
+    @Autowired
+    ShippingRepository shippingRepository;
+
     @GetMapping("/")
     public String home() {
         return "home";
@@ -36,34 +42,62 @@ public class MainController {
 
 
     @GetMapping("/products")
-    public String listProduct(Model model, @RequestParam(name = "page", defaultValue = "1") Integer page,
-                              @RequestParam(name = "categoryId", defaultValue = "-1") Integer categoryId,
-                              @RequestParam(name = "subCategoryId", defaultValue = "-1") Integer subCategoryId) {
-        final int pageSize = 20;
+    public String listProduct(HttpSession session, Model model, @RequestParam(name = "page", defaultValue = "1") int page,
+                              @RequestParam(name = "categoryId", defaultValue = "-1") int categoryId,
+                              @RequestParam(name = "subCategoryId", defaultValue = "-1") int subCategoryId) {
+
+
+        final int PAGE_SIZE = 20;
         //pageable index từ 0
-        Page<Product> products;
+        Page<Product> products = null;
 
 
         if (subCategoryId != -1) {
-            products = productRepository.findBySubCategoryId(subCategoryId, PageRequest.of(page - 1, pageSize));
+
+            products = productRepository.findBySubCategoryId(subCategoryId, PageRequest.of(0, PAGE_SIZE));
+            page = 1;
 
             //khi xài page ==> để lấy dc size page ==> objPage.getContent().size()
             if (products.getContent().size() == 0) {
-                products = productRepository.findBySubCategoryId(subCategoryId, PageRequest.of(0, pageSize));
+                products = productRepository.findBySubCategoryId(subCategoryId, PageRequest.of(0, PAGE_SIZE));
                 page = 1;
             }
 
         } else if (categoryId != -1) {
-            products = productRepository.findByCategoryId(categoryId, PageRequest.of(page - 1, pageSize));
 
-            //khi xài page ==> để lấy dc size page ==> objPage.getContent().size()
-            if (products.getContent().size() == 0) {
-                products = productRepository.findBySubCategoryId(subCategoryId, PageRequest.of(0, pageSize));
-                page = 1;
+            model.addAttribute("noCategory", "false");
+
+            String checkExistCategoryId = (String) session.getAttribute("oldCategoryId");
+            if (checkExistCategoryId == null) {
+                session.setAttribute("oldCategoryId", "-1");
+            } else {
+                int oldCategoryId = Integer.parseInt(checkExistCategoryId);
+                if (oldCategoryId != categoryId) {
+                    products = productRepository.findByCategoryId(categoryId, PageRequest.of(0, PAGE_SIZE));
+                    page = 1;
+
+
+                } else {
+                    products = productRepository.findByCategoryId(categoryId, PageRequest.of(page - 1, PAGE_SIZE));
+                }
+
+                if (products.getContent().size() == 0) {
+                    products = productRepository.findBySubCategoryId(subCategoryId, PageRequest.of(0, PAGE_SIZE));
+                    page = 1;
+                }
+                session.setAttribute("oldCategoryId", categoryId + "");
             }
 
+
+            model.addAttribute("categoryId", categoryId);
+
+
+            //khi xài page ==> để lấy dc size page ==> objPage.getContent().size()
+
+
         } else {
-            products = productRepository.findAll(PageRequest.of(page - 1, pageSize));
+            products = productRepository.findAll(PageRequest.of(page - 1, PAGE_SIZE));
+            model.addAttribute("noCategory", "true");
         }
         List<Category> categories = categoryRepository.findAll();
 
@@ -200,13 +234,13 @@ public class MainController {
                                  @RequestParam("productId") Long productId) {
         List<Cart> cartList = (List<Cart>) session.getAttribute("CART");
         for (Cart cart : cartList) {
-            if(cart.getProductId().equals(productId)) {
+            if (cart.getProductId().equals(productId)) {
                 cart.setQuantity(quantity);
                 break;
             }
         }
 
-        session.setAttribute("CART",cartList);
+        session.setAttribute("CART", cartList);
         return "redirect:/carts";
     }
 
@@ -225,27 +259,36 @@ public class MainController {
     }
 
     @GetMapping("/prepare-shipping")
-    public String prepareShipping(HttpSession session,Model model, @RequestParam("name")String fullName,
-                                  @RequestParam("phone")String phone,
-                                  @RequestParam("address")String address,
-                                  @RequestParam("note")String note) {
+    public String prepareShipping(HttpSession session, Model model, @RequestParam("name") String fullName,
+                                  @RequestParam("phone") String phone,
+                                  @RequestParam("address") String address,
+                                  @RequestParam("note") String note) {
         Shipping shipping = new Shipping();
         shipping.setName(fullName);
         shipping.setPhone(phone);
         shipping.setAddress(address);
-        model.addAttribute("note",note);
+        model.addAttribute("note", note);
 
-        List<Cart>cartList = (List<Cart>) session.getAttribute("CART");
+        session.setAttribute("SHIPPING", shipping);
+
+        List<Cart> cartList = (List<Cart>) session.getAttribute("CART");
         double totalMoney = 0;
-        for (Cart cart: cartList) {
-             totalMoney += cart.getQuantity() * cart.getProductPrice();
+        for (Cart cart : cartList) {
+            totalMoney += cart.getQuantity() * cart.getProductPrice();
         }
 
         totalMoney = Math.ceil(totalMoney * 100) / 100;
-        model.addAttribute("totalMoney",totalMoney);
-        model.addAttribute("shipping",shipping);
-        model.addAttribute("cartList",cartList);
+        model.addAttribute("totalMoney", totalMoney);
+        model.addAttribute("shipping", shipping);
+        model.addAttribute("cartList", cartList);
 
         return "prepareShipping";
+    }
+
+    @GetMapping("/thanks")
+    public String thanks(HttpSession session) {
+//        Shipping shipping = (Shipping) session.getAttribute("SHIPPING");
+//        shippingRepository.save(shipping);
+        return "thanks";
     }
 }
