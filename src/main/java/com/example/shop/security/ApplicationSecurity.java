9c3c42ea24d3e96c366889a39bf752f7a7ea3628
xuatmon01 <sonnctse140371@fpt.edu.vn>
2021-08-21 11:43:44 +0700

    update postgre database

---
+++ src/main/java/com/example/shop/security/ApplicationSecurity.java
@@ -0,0 +1,15 @@
+package com.example.shop.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        //giờ set bỏ qua đăng nhập
+        web.ignoring().antMatchers("/**");
+    }
+}
